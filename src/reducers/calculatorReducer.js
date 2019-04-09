84f15ca9ef96dc75938644230bb73e4bd121bc2c
Matthew McIntyre <mattymac2@gmail.com>
2019-04-09 20:38:46 +0100

    improved calculator logic

--- src/reducers/calculatorReducer.js
+++ src/reducers/calculatorReducer.js
@@ -1,3 +1,11 @@
+import {
+  EQUALS,
+  ALL_CLEAR,
+  ADD_NUMBER,
+  ADD_DECIMAL,
+  OPERATOR
+} from "../actions/actionTypes";
+
 const initialState = {
   //what is displayed on screen
   currentOutput: "0",
@@ -6,45 +14,66 @@ const initialState = {
   valueOne: "",
 
   //operator in question goes here
-  operator: ""
+  //when this exists, change color of buttons...?
+  currentOperator: ""
 };
 
 const calculatorReducer = (state = initialState, action) => {
   switch (action.type) {
     //TODO equals must output a max number of digits (14)
-    case "EQUALS":
+    case EQUALS:
+      // eslint-disable-next-line no-eval
+      let myResult = eval(
+        state.valueOne + state.currentOperator + state.currentOutput
+      );
+
       return {
         ...state,
-        // eslint-disable-next-line no-eval
-        currentOutput: eval(state.currentOutput)
+        currentOutput: String(myResult),
+        currentOperator: ""
       };
 
-    case "ALLCLEAR":
+    case ALL_CLEAR:
       return {
         ...state,
         currentOutput: "0",
-        newValue: false
+        valueOne: "",
+        currentOperator: ""
       };
 
     //TODO addnumber needs to do the following
-    //1. get rid of zero if it is the first number
+    //1. get rid of zero if it is the first number 80% done - if 0 is pressed it will disappear
     //2. max number of digits to fit on screen
-    case "ADDNUMBER":
+    case ADD_NUMBER:
+      let result = state.currentOutput + action.number;
+
+      console.log(state.currentOperator);
+      console.log(state.currentOutput);
       return {
         ...state,
 
-        currentOutput: state.currentOutput + action.number
+        currentOutput: result.replace(/^0+/, "")
       };
 
-    //TODO add decimal..
-    //this must only add ONE decimal and refuse any further ...regex?
+    ///https://stackoverflow.com/questions/27628746/need-a-regexp-to-filter-out-all-but-one-decimal-point
+    //THANKYOUUUUUUUU ^
+    case ADD_DECIMAL:
+      let newDecimal = state.currentOutput + action.decimal;
+
+      return {
+        ...state,
+
+        currentOutput: newDecimal.replace(/[^0-9.]|\.(?=.*\.)/, "")
+      };
 
     //TODO
     //1. operator must change outline of the operator button
-    case "OPERATOR":
+    case OPERATOR:
       return {
         ...state,
-        currentOutput: state.currentOutput + " " + action.operator + " "
+        currentOperator: action.operator,
+        valueOne: state.currentOutput,
+        currentOutput: ""
       };
 
     default:
